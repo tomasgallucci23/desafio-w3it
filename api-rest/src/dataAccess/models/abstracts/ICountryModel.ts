7207cb61b9402b8eabc3042ddf460f045b917560
Tomas Gallucci <admin@dativasolutions.com>
2023-11-17 10:56:19 -0300

    feat(ICountryModel): add interfaces to model

--- api-rest/src/dataAccess/models/abstracts/ICountryModel.ts
+++ api-rest/src/dataAccess/models/abstracts/ICountryModel.ts
@@ -1,5 +1,5 @@
 export default interface ICountryModel {
-  _id: string;
+  _id: number;
   name: string;
   population: number;
 }
--- api-rest/src/dataAccess/models/implementation/CountryModel.ts
+++ api-rest/src/dataAccess/models/implementation/CountryModel.ts
@@ -1,11 +1,11 @@
 import ICountryModel from "../abstracts/ICountryModel";
 
 export default class CountryModel implements ICountryModel {
-  _id: string;
+  _id: number;
   name: string;
   population: number;
 
-  constructor(id: string, name: string, population: number) {
+  constructor(id: number, name: string, population: number) {
     this._id = id;
     this.name = name;
     this.population = population;
